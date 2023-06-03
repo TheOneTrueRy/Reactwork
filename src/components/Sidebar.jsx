c5e942f0c77c141fff3c09cadfe6168fefe6788e
Ry <OfficiallyRy@outlook.com>
2023-06-02 18:02:32 -0600

    Fixed class and graduate text size & placement

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -22,8 +22,8 @@ function Sidebar() {
         <div className="col-12 d-flex align-items-center justify-content-center top">
           <img src={account.picture} alt="account photo" height="200" width="200" className="rounded-circle border border-2 border-dark elevation-1" />
         </div>
-        <div className="col-12 pt-4 px-4 pb-5">
-          <span className="fs-3">{account.class} {account.graduated ? 'Graduate' : ''}</span>
+        <div className="col-12 text-center pt-4 px-4 pb-5">
+          <span className="fs-5">{account.class} {account.graduated ? 'Graduate' : ''}</span>
           <div className="text-center">
           <span className="fs-3 fw-bold">{account.name}</span>
           </div>
