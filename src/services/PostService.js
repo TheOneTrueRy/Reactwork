d240f276bcf579bc86383986f1616d4518238f15
Ry <OfficiallyRy@outlook.com>
2023-06-07 15:48:51 -0600

    No longer need loggers

--- src/services/PostService.js
+++ src/services/PostService.js
@@ -1,6 +1,5 @@
 import { AppState } from "../AppState.js"
 import { Post } from "../models/Post.js"
-import { logger } from "../utils/Logger.js"
 import { api } from "./AxiosService.js"
 
 class PostService {
@@ -13,7 +12,6 @@ class PostService {
     const res = await api.get(url)
     const additionalPosts = res.data.posts.map(p => new Post(p))
     AppState.posts = [...AppState.posts, ...additionalPosts]
-    logger.log(AppState.posts)
     AppState.olderPosts = res.data.older
   }
 
@@ -25,7 +23,6 @@ class PostService {
 
   async likePost(postId) {
     const res = await api.post(`/api/posts/${postId}/like`)
-    logger.log(res.data)
     let postIndex = AppState.posts.findIndex(post => post.id == postId)
     AppState.posts.splice(postIndex, 1, new Post(res.data))
   }
