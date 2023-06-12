5dd720abf7e82834fba315e714e0811d48f15d31
Ry <OfficiallyRy@outlook.com>
2023-06-12 17:23:58 -0600

    oopsie turns out I will need this teehee

---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,14 @@
+import { observer } from 'mobx-react-lite';
+import React from 'react';
+
+function ProfileCard() {
+
+  return (
+
+    <div className="ProfileCard">
+
+    </div>
+  )
+
+}
+export default observer(ProfileCard)
